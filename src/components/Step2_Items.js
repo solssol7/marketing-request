804328500e16e09dd3e467a65b1cf889702a05b8
solssol7 <sol@aswemake.com>
2026-01-28 11:05:25 +0900

    Update Step2_Items.js

--- src/components/Step2_Items.js
+++ src/components/Step2_Items.js
@@ -9,11 +9,14 @@ export default function Step2_Items({
     const tabs = ['X배너', '현수막', '전단지', '기타', '디자인', '자료실'];
     const tabIds = ['tab1', 'tab2', 'tab3', 'tab4', 'tab5', 'tab6'];
 
+    // X배너 (1~16번), 현수막 (1~6번) 배열 생성
+    const xBannerList = Array.from({ length: 16 }, (_, i) => i + 1);
+    const bannerList = Array.from({ length: 6 }, (_, i) => i + 1);
+
     return (
         <div className={common.stepContent}>
             <h2 className={common.title}>{selectedMart?.name}에 필요한 물품은?</h2>
             
-            {/* [New] 가이드 텍스트 */}
             <p className={common.guideText}>
                 2. 필요한 품목을 선택하면 여러 종류를 한꺼번에 신청이 가능합니다.
             </p>
@@ -30,7 +33,7 @@ export default function Step2_Items({
             </div>
 
             <div className={styles.tabBody}>
-                {/* Tab 1: X배너 */}
+                {/* Tab 1: X배너 (16종) */}
                 {activeTab === 'tab1' && (
                     <div>
                         <div className={common.row}>
@@ -43,9 +46,9 @@ export default function Step2_Items({
                             <div className={styles.selectedSummary}>선택됨: {currentRequest.x배너디자인.length > 0 ? currentRequest.x배너디자인.join(', ') : '없음'}</div>
                         </div>
                         <div className={`${styles.imageGrid} ${common.desktopOnly}`}>
-                            {[1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+                            {xBannerList.map(num => (
                                 <label key={num} className={`${styles.imgLabel} ${currentRequest.x배너디자인.includes(`type${num}`) ? styles.selectedImg : ''}`}>
-                                    <img src={`https://fs.qmk.me/template-xbanner-${num}.png`} alt={`디자인 ${num}`} />
+                                    <img src={`https://fs.qmk.me/template-xbanner-${num}-202601.webp`} alt={`디자인 ${num}`} loading="lazy" />
                                     <input type="checkbox" checked={currentRequest.x배너디자인.includes(`type${num}`)} onChange={() => handleXBannerCheck(`type${num}`)} hidden />
                                     <span>{num}번</span>
                                 </label>
@@ -54,7 +57,7 @@ export default function Step2_Items({
                     </div>
                 )}
 
-                {/* Tab 2: 현수막 */}
+                {/* Tab 2: 현수막 (6종) */}
                 {activeTab === 'tab2' && (
                     <div>
                         <div className={common.row}>
@@ -67,11 +70,11 @@ export default function Step2_Items({
                             <div className={styles.selectedSummary}>선택됨: {currentRequest.현수막디자인 || '미선택'}</div>
                         </div>
                         <div className={`${styles.bannerGrid} ${common.desktopOnly}`}>
-                            {['type1', 'type2'].map((type, idx) => (
-                                <label key={type} className={`${styles.bannerLabel} ${currentRequest.현수막디자인 === type ? styles.selectedBanner : ''}`}>
-                                    <input type="radio" name="banner" value={type} checked={currentRequest.현수막디자인 === type} onChange={() => handleBannerTypeChange(type)} hidden />
-                                    <img src={`https://fs.qmk.me/template-banner-${idx + 1}.png`} alt={`디자인 ${idx + 1}`} onError={(e) => e.target.src='https://placehold.co/400x100?text=Design'} />
-                                    <span>디자인 {idx + 1}</span>
+                            {bannerList.map((num) => (
+                                <label key={num} className={`${styles.bannerLabel} ${currentRequest.현수막디자인 === `type${num}` ? styles.selectedBanner : ''}`}>
+                                    <input type="radio" name="banner" value={`type${num}`} checked={currentRequest.현수막디자인 === `type${num}`} onChange={() => handleBannerTypeChange(`type${num}`)} hidden />
+                                    <img src={`https://fs.qmk.me/template-banner-${num}-202601.webp`} alt={`디자인 ${num}`} onError={(e) => e.target.src='https://placehold.co/600x150?text=Design'} loading="lazy" />
+                                    <span>디자인 {num}</span>
                                 </label>
                             ))}
                         </div>
@@ -83,7 +86,7 @@ export default function Step2_Items({
                 {activeTab === 'tab4' && <textarea className={common.textarea} placeholder="기타 요청사항을 자세히 적어주세요." value={currentRequest.기타} onChange={(e) => handleTextChange(e, '기타')}></textarea>}
                 {activeTab === 'tab5' && <div className={common.row}><label className={common.label}>용도 <input type="text" className={common.input} value={currentRequest.디자인용도} onChange={(e) => handleTextChange(e, '디자인용도')}/></label><label className={common.label}>사이즈 <input type="text" className={common.input} value={currentRequest.디자인사이즈} onChange={(e) => handleTextChange(e, '디자인사이즈')}/></label></div>}
                 
-                {/* Tab 6: 자료실 (QR 다운로드 및 링크) */}
+                {/* Tab 6: 자료실 (링크 수정됨) */}
                 {activeTab === 'tab6' && (
                     <div className={styles.refContainer}>
                         <div className={styles.refDesc}>
@@ -92,11 +95,10 @@ export default function Step2_Items({
                         
                         <div className={styles.qrWrapper}>
                             <img src="/qrcode.png" alt="Google Drive QR Code" className={styles.qrImage} />
-                            {/* [New] 다운로드 버튼 */}
                             <a href="/qrcode.png" download="qrcode.png" className={styles.downloadBtn}>⬇ QR 다운로드</a>
                         </div>
                         
-                        <a href="https://drive.google.com/drive/folders/187vViWyscfKTP9s1DmCCAQvU6_IB3U-Z" target="_blank" className={styles.driveLinkBtn}>
+                        <a href="https://drive.google.com/drive/folders/1LCkZ-fryH7aWBll2zH_qp9NwfalGKQU0" target="_blank" className={styles.driveLinkBtn}>
                             📂 구글 드라이브 열기
                         </a>
                     </div>
